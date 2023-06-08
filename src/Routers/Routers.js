03ca9dae745134611f9ce51be6bc1572398a0586
R7558 <rupsanaworld1004@gmail.co>
2023-06-08 23:04:44 +0600

    added about

--- src/Routers/Routers.js
+++ src/Routers/Routers.js
@@ -41,6 +41,7 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import About from "../pages/About/About";
 // import Home from "../pages/Home";
 // import About from "../pages/About";
 // import CarListing from "../pages/CarListing";
@@ -55,13 +56,13 @@ const Routers = () => {
     <Routes>
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<Home />} />
-      {/* <Route path="/about" element={<About />} />
-      <Route path="/cars" element={<CarListing />} />
-      <Route path="/cars/:slug" element={<CarDetails />} />
-      <Route path="/blogs" element={<Blog />} />
-      <Route path="/blogs/:slug" element={<BlogDetails />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="*" element={<NotFound />} /> */}
+      <Route path="/about" element={<About />} />
+      {/* <Route path="/cars" element={<CarListing />} /> */}
+      {/* <Route path="/cars/:slug" element={<CarDetails />} /> */}
+      {/* <Route path="/blogs" element={<Blog />} /> */}
+      {/* <Route path="/blogs/:slug" element={<BlogDetails />} /> */}
+      {/* <Route path="/contact" element={<Contact />} /> */}
+      {/* <Route path="*" element={<NotFound />} /> */}
     </Routes>
   );
 };
