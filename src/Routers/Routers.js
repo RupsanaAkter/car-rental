e83941fd7b6f8ff4501d4eef672783babaf72389
R7558 <rupsanaworld1004@gmail.co>
2023-06-08 23:21:22 +0600

    added blogdetails

--- src/Routers/Routers.js
+++ src/Routers/Routers.js
@@ -43,6 +43,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import About from "../pages/About/About";
 import Blog from "../pages/Blog/Blog";
+import BlogDetails from "../pages/BlogDetails/BlogDetails";
 // import Home from "../pages/Home";
 // import About from "../pages/About";
 // import CarListing from "../pages/CarListing";
@@ -61,7 +62,7 @@ const Routers = () => {
       {/* <Route path="/cars" element={<CarListing />} /> */}
       {/* <Route path="/cars/:slug" element={<CarDetails />} /> */}
       <Route path="/blogs" element={<Blog />} />
-      {/* <Route path="/blogs/:slug" element={<BlogDetails />} /> */}
+      <Route path="/blogs/:slug" element={<BlogDetails />} />
       {/* <Route path="/contact" element={<Contact />} /> */}
       {/* <Route path="*" element={<NotFound />} /> */}
     </Routes>
